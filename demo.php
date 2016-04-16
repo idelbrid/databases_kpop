0df1083d5c650e93fdb56895c4ed34846eb4fd19
Ian Delbridge <idelbrid@u.rochester.edu>
2016-04-16 18:49:05 -0400

    Successful connection to database

--- demo.php
+++ demo.php
@@ -2,8 +2,9 @@
 require_once './dbsetup.php';
 
 date_default_timezone_set("America/New_York");
-$sql = "SELECT COUNT * FROM artist";
-$count = $db->query($sql);
+$sql = "SELECT COUNT(*) FROM artist";
+$result = $db->query($sql);
+$count = $result->fetchColumn();
 ?>
 
 <html>
@@ -14,7 +15,7 @@ $count = $db->query($sql);
 		<h2>Welcome to the K-pop database website!</h2>
 		<p>
 			We have all sorts of data about so much K-pop somehow. We have over
-			<?php ?>
+			<?php echo $count?>
 		</p>
 	</body>
 
