9a486475d0106fc9f27028598060a348f506392e
Ian Delbridge <idelbrid@u.rochester.edu>
2016-04-23 18:59:02 -0400

    links to both song-list and fanclub-list. altered yichens data load to fit idelbrid_db

--- index.php
+++ index.php
@@ -7,6 +7,21 @@ $result = $db->query($sql);
 $count = $result->fetchColumn();
 ?>
 
+<style>
+    ul#menu li {
+        display: inline;
+    }
+    ul#menu li a {
+        color: WHITE;
+        background-color: #33CCFF;
+        padding: 10px 20px;
+        text-decoration: none;
+    }
+    ul#menu li a:hover{
+        background-color: cadetblue;
+    }
+</style>
+
 <html>
 <head>
     <title>K-pop Home</title>
@@ -14,6 +29,13 @@ $count = $result->fetchColumn();
 <body>
 <h1 style="text-align:center;"><a href="index.php">K-Pop Database</a></h1>
 <hr>
+
+<ul id="menu">
+    <li><a href="./song-list.php">Songs</a></li>
+    <li><a href="./fanclub-list.php">Fanclubs</a></li>
+<!--    <li><a href="/js/default.asp">JavaScript</a></li>-->
+<!--    <li><a href="/php/default.asp">PHP</a></li>-->
+</ul>
 <h2>Welcome to the K-pop database website!</h2>
 <p>
     We have all sorts of data about so much K-pop somehow. We have over
