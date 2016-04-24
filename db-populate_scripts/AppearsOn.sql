fa50c79f5cd76c656f735df068441114b9a71275
kaychn91 <annie.he91@gmail.com>
2016-04-24 13:21:37 -0400

    Added files via upload

---
+++ db-populate_scripts/AppearsOn.sql
@@ -0,0 +1,27 @@
+/* Appears On:
+artists we have: g-dragon, sung si-kyung, kim jin-pyo, yoon do-hyun, lena park, psy
+shows we have: family outing, happy together, strong heart, infinity challenge, folden fishery, star king, hello baby, running man 
+appears_on(artist_name, show_name)*/
+
+INSERT INTO appears_on VALUES
+('G-Dragon', 'Running Man'),
+('G-Dragon', 'Family Outing'),
+('G-Dragon', 'Happy Together'),
+('G-Dragon', 'Infinity Challenge'),
+('G-Dragon', 'Star King'),
+('G-Dragon', 'Strong Heart'),
+('Sung Si-kyung', 'Running Man'),
+('Sung Si-kyung', 'Infinity Challenge'),
+('Sung Si-kyung', 'Happy Together'),
+('Sung Si-kyung', 'Strong Heart'),
+('Yoo Do-hyun', 'Running Man'),
+('PSY', 'Running Man'),
+('PSY', 'Strong Heart'),
+('PSY', 'Star King'),
+('PSY', 'Infinity Challenge'),
+('PSY', 'Happy Together');
+
+
+
+
+
