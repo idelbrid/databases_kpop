5b59e985eb2f71692b3480eaf7333d86a6177b33
Ian Delbridge <idelbrid@u.rochester.edu>
2016-04-25 19:05:31 -0400

    debugged ranks.sql

--- db-populate_scripts/ranks.sql
+++ db-populate_scripts/ranks.sql
@@ -9,28 +9,30 @@ albums we have: bigbang vol.1 big, made series, first step, 2get
 */
 START TRANSACTION;
 
-INSERT INTO song VALUES
-('Toy', 'Blooming Period', 'Block B', 'KOR', interval '00:03:27', date('2016-04-10'), null, TRUE, 'K-pop', TRUE),
-('Talk Love', 'Descendants of the Sun', 'K. Will', 'KOR', interval '00:03:37', date('2016-03-17'), null, TRUE, 'Ballad', TRUE),
-('This Love', 'Amaranth', 'Davichi','KOR', interval '00:03:49', date('2016-03-02'), null, TRUE, 'Ballad', TRUE),
-("You're the Best", 'Melting', 'Mamamoo', 'KOR', interval '00:03:52', date('2016-02-25'), null, TRUE, 'K-pop', TRUE),
-("You're So Fine", 'CNBLUE', 'BLUEMING', 'KOR', interval '00:03:52', date('2016-04-03'), null, TRUE, 'K-pop', TRUE);
-
 
 INSERT INTO album VALUES
 ('Blooming Period', 'Block B', date('2016-04-10'), 39800, 'K-pop'),
 ('BLUEMING', 'CNBLUE', date('2016-04-04'), 49064, 'Indie'),
-('Flight Log: Departure', 'GOT7', date('2016-03-21'), 99977, 'R&B');
+('Flight Log: Departure', 'GOT7', date('2016-03-21'), 99977, 'R&B'),
+('Descendants of the Sun', 'K.Will', NULL, NULL, 'K-pop'),
+('Amaranth', 'Davichi', date('2008-01-28'), NULL, 'R&B');
+
+INSERT INTO song VALUES
+('Toy', 'Blooming Period', 'Block B', 'KOR', interval '00:03:27', date('2016-04-10'), null, TRUE, 'K-pop', TRUE),
+('Talk Love', 'Descendants of the Sun', 'K.Will', 'KOR', interval '00:03:37', date('2016-03-17'), null, TRUE, 'Ballad', TRUE),
+('This Love', 'Amaranth', 'Davichi','KOR', interval '00:03:49', date('2016-03-02'), null, TRUE, 'Ballad', TRUE),
+('You''re the Best', 'Melting', 'Mamamoo', 'KOR', interval '00:03:52', date('2016-02-25'), null, TRUE, 'K-pop', TRUE),
+('You''re So Fine', 'BLUEMING', 'CNBLUE', 'KOR', interval '00:03:52', date('2016-04-03'), null, TRUE, 'K-pop', TRUE);
 
 INSERT INTO song_ranks VALUES
 
 (2, 'Toy', 'Block B', 'Blooming Period', date('2016-04-10')),
-(4, 'Talk Love', 'K. Will', 'Descendants of the Sun', date('2016-04-10')),
+(4, 'Talk Love', 'K.Will', 'Descendants of the Sun', date('2016-04-10')),
 (5, 'This Love', 'Davichi', 'Amaranth', date('2016-04-10')),
 (1, 'Toy', 'Block B', 'Blooming Period', date('2016-04-03')),
-(2, 'Talk Love', 'K. Will', null, date('2016-04-03')),
-(16, "You're the Best", 'Mamamoo', 'Melting', date('2016-04-03')),
-(19, "You're So Fine", 'CNBLUE', 'BLUEMING', date('2016-04-03'));
+(2, 'Talk Love', 'K.Will', null, date('2016-04-03')),
+(16, 'You''re the Best', 'Mamamoo', 'Melting', date('2016-04-03')),
+(19, 'You''re So Fine', 'CNBLUE', 'BLUEMING', date('2016-04-03'));
 
 
 
