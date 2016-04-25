56131721b0a18421b384ad9abded92b21814d169
Annie He <anniehe@dhcp-10-5-25-33.wireless.rochester.edu>
2016-04-25 15:38:57 -0400

    fixed dates

--- db-populate_scripts/AppearsOn.sql
+++ db-populate_scripts/AppearsOn.sql
@@ -6,21 +6,21 @@ appears_on(artist_name, show_name, episode_date)*/
 ALTER TABLE appears_on ADD COLUMN episode_date date;
 
 INSERT INTO appears_on VALUES
-('G-Dragon', 'Running Man', 2013-09-15),
-('G-Dragon', 'Family Outing', 2008-08-03),
-('G-Dragon', 'Happy Together', 2015-05-21),
-('G-Dragon', 'Infinity Challenge', 2015-08-22),
-('G-Dragon', 'Infinity Challenge', 2015-08-08),
-('G-Dragon', 'Star King', 2007-03-11),
-('G-Dragon', 'Strong Heart', 2009-10-06),
-('Sung Si-kyung', 'Running Man', 2015-03-22),
-('Sung Si-kyung', 'Happy Together', 2014-01-15),
-('Sung Si-kyung', 'Strong Heart', 2011-09-20),
-('Yoo Do-hyun', 'Running Man', 2012-07-01),
-('PSY', 'Running Man', 2015-07-03),
-('PSY', 'Star King', 2013-02-02),
-('PSY', 'Infinity Challenge', 2011-07-02),
-('PSY', 'Happy Together', 2009-12-03);
+('G-Dragon', 'Running Man', date('2013-09-15')),
+('G-Dragon', 'Family Outing', date('2008-08-03')),
+('G-Dragon', 'Happy Together', date('2015-05-21')),
+('G-Dragon', 'Infinity Challenge', date('2015-08-22')),
+('G-Dragon', 'Infinity Challenge', date('2015-08-08')),
+('G-Dragon', 'Star King', date('2007-03-11')),
+('G-Dragon', 'Strong Heart', date('2009-10-06')),
+('Sung Si-kyung', 'Running Man', date('2015-03-22')),
+('Sung Si-kyung', 'Happy Together', date('2014-01-15')),
+('Sung Si-kyung', 'Strong Heart', date('2011-09-20')),
+('Yoo Do-hyun', 'Running Man', date('2012-07-01')),
+('PSY', 'Running Man', date('2015-07-03')),
+('PSY', 'Star King', date('2013-02-02')),
+('PSY', 'Infinity Challenge', date('2011-07-02')),
+('PSY', 'Happy Together', date('2009-12-03'));
 
 
 
