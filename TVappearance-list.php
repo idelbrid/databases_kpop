4aadb2f8230dae08287db7c9289f56db77e8493a
Annie He <anniehe@dhcp-10-5-28-13.wireless.rochester.edu>
2016-04-28 15:40:40 -0400

    updated TV appearance list

--- TVappearance-list.php
+++ TVappearance-list.php
@@ -8,7 +8,7 @@ require_once './dbsetup.php';
 	</head>
 	<body>
 	<?php
-	$stmt = "SELECT artist_name, show_name, episode_date FROM appears_on ORDER BY artist_name, show_name, episode_date";
+	$stmt = "SELECT artist_name, show_name, episode_date FROM appears_on ORDER BY artist_name, show_name, episode_date;";
 	if(!$stmt){
 		echo "PDO::errorInfo():\n";
 		print_r($db->errorInfo());
@@ -18,18 +18,20 @@ require_once './dbsetup.php';
 	<?php include_once './header.php'?>
 		<h2>TV Show Appearances</h2>
 		<section id="search">
+			<p> 
+			Look up what TV shows your favorite K-pop idols have appeared in!
+			</p>
 			<form action="./TVappearance-show.php" method="get">
-				<label>Search artist: <input type="text" name="artist_name" required></label>
-			
-				<label> Search show: <input type="text" name="show_name" required></label>
-				<input type="submit" value ="submit">
+				<label>Artist name: <input type="text" name="name" required></label>
+				<input type="submit" value="Search">
 			</form>
 		</section>
 		<table class="table">
 		<tr>
 			<th>Artist</th>
 			<th>TV Show</th>
-			<th>Date</th>
+			<th> Episode Dates </th>
+			<th>Link to Details</th>
 		</tr>
 		<?php
 			foreach ($db->query($stmt) as $row)
@@ -37,10 +39,10 @@ require_once './dbsetup.php';
 				$artist_name = $row['artist_name'];
 				$show_name = $row['show_name'];
 				$episode_date = $row['episode_date'];
-   			//$link = "./TVappearance-show.php?name=";
-			echo "<tr><td>$artist_name</td><td>$show_name</td><td>$episode_date</td></tr>";
-			//echo <tr><td>$row['artist']</td><td>$row['name']</td><td>lala</td></tr>";
+				$link = "./TVappearance-show.php?name=";
+			echo "<tr><td>$artist_name</td><td>$show_name</td><td>$episode_date</td><td><a href=$link" . urlencode($artist_name) .">details</a></td></tr>";
 			}
 		?>
+		
 		</table>
 	<?php include_once './footer.php'?>
